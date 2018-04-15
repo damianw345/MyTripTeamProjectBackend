c3b7f9c8e2a1b0772a094e33020a73edff02a0b1
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-04-15 14:37:57 +0200

    Dodanie gradle wrappera i usuniecie default value dla timestampow

--- mytrip/src/main/resources/db/migration/V1__create_database.sql
+++ mytrip/src/main/resources/db/migration/V1__create_database.sql
@@ -2,8 +2,8 @@ CREATE TABLE `trip` (
 	`trip_id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` varchar(255) NOT NULL,
 	`description` TEXT,
-	`start` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01',
-	`end` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01',
+	`start` TIMESTAMP NOT NULL,
+	`end` TIMESTAMP NOT NULL,
 	`poster` varchar(1000) UNIQUE,
 	`presentation` varchar(1000) UNIQUE
 );
@@ -13,14 +13,14 @@ CREATE TABLE `waypoint` (
 	`trip_id` BIGINT NOT NULL,
 	`latitude` DECIMAL(10,8) NOT NULL,
 	`longitude` DECIMAL(11,8) NOT NULL,
-	`date` TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:01',
+	`date` TIMESTAMP NOT NULL,
 	FOREIGN KEY (`trip_id`) REFERENCES `trip`(`trip_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `photo` (
 	`photo_id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`waypoint_id` BIGINT NOT NULL,
-	`date` TIMESTAMP DEFAULT '1970-01-01 00:00:01',
+	`date` TIMESTAMP,
 	`url` varchar(1000) NOT NULL UNIQUE,
 	`thumbnail_url` varchar(1000) NOT NULL UNIQUE,
 	FOREIGN KEY (`waypoint_id`) REFERENCES `waypoint`(`waypoint_id`) ON DELETE CASCADE
@@ -29,7 +29,7 @@ CREATE TABLE `photo` (
 CREATE TABLE `video` (
 	`video_id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`waypoint_id` BIGINT NOT NULL,
-	`date` TIMESTAMP DEFAULT '1970-01-01 00:00:01',
+	`date` TIMESTAMP,
 	`url` varchar(1000) NOT NULL UNIQUE,
 	FOREIGN KEY (`waypoint_id`) REFERENCES `waypoint`(`waypoint_id`) ON DELETE CASCADE
 );
