dadf7109b791a8d5f62ec218116286b7c61c4f6f
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-06-05 00:50:59 +0200

    Konfiguracja CORSa

--- mytrip/src/main/java/pl/mytrip/configuration/SecurityConfig.java
+++ mytrip/src/main/java/pl/mytrip/configuration/SecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pl.mytrip.user.MyTripUserDetailsService;
 import pl.mytrip.user.UserRepository;
 
@@ -51,7 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().disable()
+                .cors().and()
                 .authorizeRequests()
                     .antMatchers("/api/**").fullyAuthenticated()
                 .and()
@@ -60,5 +63,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .addFilter(baseAuthFilter());
     }
-
 }
