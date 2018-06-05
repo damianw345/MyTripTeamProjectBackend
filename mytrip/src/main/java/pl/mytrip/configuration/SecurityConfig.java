ac06b1dfcc3fd1b62237206547a5b030c0db4f83
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-06-05 21:24:15 +0200

    Konfiguracja CORSa pod Swaggera

--- mytrip/src/main/java/pl/mytrip/configuration/SecurityConfig.java
+++ mytrip/src/main/java/pl/mytrip/configuration/SecurityConfig.java
@@ -13,9 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pl.mytrip.user.MyTripUserDetailsService;
 import pl.mytrip.user.UserRepository;
 
@@ -54,13 +51,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().and()
                 .authorizeRequests()
                     .antMatchers("/api/**").fullyAuthenticated()
                 .and()
                     .httpBasic()
                     .authenticationEntryPoint(authEntryPoint)
                 .and()
-                    .addFilter(baseAuthFilter());
+                    .addFilter(baseAuthFilter())
+                    .addFilterAfter(
+                        new CorsFilter(), BasicAuthenticationFilter.class);
     }
 }
